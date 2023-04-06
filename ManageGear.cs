cb4935b06358234256d431f475cb5407b0cb3d74
TealTime <>
2023-04-06 00:41:20 -0700

    Addressed a potential NullReference codepath
    Previous implementation added a check case to catch if the world started
    to burn. This change sought to make the world burn a little less
    intensely.

--- ManageGear.cs
+++ ManageGear.cs
@@ -317,6 +317,9 @@ namespace XRL.World.CleverGirl {
         /// <summary>
         /// BodyPart had a typo in one of its field names. The devs fixed this typo in [2.0.204.65].
         /// This functions serves as a backwards compatible accessor until beta becomes stable.
+        /// <returns>
+        /// false if the processed part is NOT the preferred primary weapon, true otherwise
+        /// </returns>
         /// </summary>
         public static bool CheckPreferredPrimary(BodyPart part) {
             // TODO: Remove this once [2.0.204.65] is long considered stable.
@@ -324,6 +327,10 @@ namespace XRL.World.CleverGirl {
                              part.GetType().GetField("PreferedPrimary");
             if (prop == null) {
                 Utility.MaybeLog("Could not find PreferredPrimary field in BodyPart. This could be critical?");
+
+                // This return will expend the player's action turn when it might not need to, but the potential NullReference error
+                // codepath below is debatively worse.
+                return false;
             }
             return (bool)prop.GetValue(part);
         }
